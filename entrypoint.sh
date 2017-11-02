7d92e6b8fb67a0f146b934273de02abd8b447c4d
toshi-click <toshi@toshi.click>
2017-11-02 11:37:28 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,4 @@ export MAIN_CLASS=$(unzip -p schemaspy*.jar META-INF/MANIFEST.MF | grep Main-Cla
 echo "Running Main-Class $MAIN_CLASS"
 echo -n "With drivers:"
 ls -Ax $DRIVER_PATH | sed -e 's/  */, /g'
+tail -f /dev/null
